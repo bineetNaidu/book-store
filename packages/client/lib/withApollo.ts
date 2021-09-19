e03ba5569432bd13611a01239c57dbda3b946d60
bineetNaidu <bineet.s.naidu@gmail.com>
2021-09-19 13:15:24 +0530

    ✨ setup apollo client

---
+++ packages/client/lib/withApollo.ts
@@ -0,0 +1,20 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { NextPageContext } from 'next';
+import { withApollo as createWithApollo } from 'next-apollo';
+
+const client = (ctx?: NextPageContext) =>
+  new ApolloClient({
+    // uri: process.env.NEXT_PUBLIC_API_URL + '/graphql',
+    uri: 'http://localhost:4000/graphql',
+    credentials: 'include',
+
+    headers: {
+      cookie:
+        (typeof window === 'undefined'
+          ? ctx?.req?.headers.cookie
+          : undefined) || '',
+    },
+    cache: new InMemoryCache(),
+  });
+
+export const withApollo = createWithApollo(client);
