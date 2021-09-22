43859c1bab0b50a69847b75c70c26f1e959118ff
bineetNaidu <bineet.s.naidu@gmail.com>
2021-09-22 23:16:47 +0530

    ❇️ add Field Resolvers for Book Resolver

--- packages/api/src/resolvers/Books.ts
+++ packages/api/src/resolvers/Books.ts
@@ -1,8 +1,22 @@
 import { Book } from '../entities/Book';
-import { Query, Resolver, Arg } from 'type-graphql';
+import { Query, Resolver, Arg, FieldResolver, Root } from 'type-graphql';
+import { Author } from '../entities/Author';
+import { Genre } from '../entities/Genre';
 
-@Resolver()
+@Resolver(Book)
 export class BookResolver {
+  @FieldResolver(() => Author)
+  async author(@Root() book: Book): Promise<Author> {
+    const author = await Author.findOne({ where: { id: book.authorId } });
+    return author!;
+  }
+
+  @FieldResolver(() => Genre)
+  async genre(@Root() book: Book): Promise<Genre> {
+    const genre = await Genre.findOne({ where: { id: book.genreId } });
+    return genre!;
+  }
+
   @Query(() => [Book])
   async books(): Promise<Book[]> {
     return Book.find();
